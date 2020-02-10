2118e1887387345ff9513cc3b8965e27d5c14051
Rodrigo <rodrigo.mendonca81@gmail.com>
2020-02-09 23:54:14 -0300

    cleanup

--- lego.py
+++ lego.py
@@ -2,7 +2,7 @@
 import time
 from time import sleep
 
-from pylgbst import *
+from pylgbst import logging
 from pylgbst.hub import MoveHub
 from pylgbst.peripherals import EncodedMotor, TiltSensor, Current, Voltage, COLORS, COLOR_BLACK
 from pylgbst.hub import VisionSensor
@@ -10,56 +10,6 @@ from pylgbst.hub import VisionSensor
 log = logging.getLogger("demo")
 
 data_distance = 0
-
-DEMO_CHOICES = {
-    
-}
-
-def get_options():
-    import argparse
-    arg_parser = argparse.ArgumentParser(
-        description='Demonstrate move-hub communications',
-    )
-    arg_parser.add_argument(
-        '-c', '--connection',
-        default='auto://',
-        help='''Specify connection URL to use, `protocol://mac?param=X` with protocol in:
-    "gatt","pygatt","gattlib","gattool", "bluepy","bluegiga"'''
-    )
-    arg_parser.add_argument(
-        '-d', '--demo',
-        default='all',
-        choices=sorted(DEMO_CHOICES.keys()),
-        help="Run a particular demo, default all"
-    )
-    return arg_parser
-
-def connection_from_url(url):
-    import pylgbst
-    if url == 'auto://':
-        return None
-    try:
-        from urllib.parse import urlparse, parse_qs
-    except ImportError:
-        from urlparse import urlparse, parse_qs
-    parsed = urlparse(url)
-    name = 'get_connection_%s' % parsed.scheme
-    factory = getattr(pylgbst, name, None)
-    if not factory:
-        msg = "Unrecognised URL scheme/protocol, expect a get_connection_<protocol> in pylgbst: %s"
-        raise ValueError(msg % parsed.protocol)
-    params = {}
-    if parsed.netloc.strip():
-        params['hub_mac'] = parsed.netloc
-    for key, value in parse_qs(parsed.query).items():
-        if len(value) == 1:
-            params[key] = value[0]
-        else:
-            params[key] = value
-    return factory(
-        **params
-    )
-
 
 def move(movehub):
     #log.info("Motors movement demo: angled")
@@ -72,16 +22,7 @@ def stop(movehub):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    parser = get_options()
-    options = parser.parse_args()
-    parameters = {}
-    try:
-        connection = connection_from_url(options.connection)
-        parameters['connection'] = connection
-    except ValueError as err:
-        parser.error(err.args[0])
-
-    hub = MoveHub(**parameters)
+    hub = MoveHub()
 
     try:
         def callback(clr, distance):
